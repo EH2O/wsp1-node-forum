bc431c7cb4b37e8fd30aa5a81e34c2a2acfae0f8
EH2O <edwin.holgerssonolofsson@elev.ga.ntig.se>
2023-01-20 11:13:50 +0100

    1234567890+

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser')
 const nunjucks = require('nunjucks');
 const app = express();
 const port = 3000;
+
 
 const indexRouter = require('./routes/index');
 
@@ -11,8 +13,12 @@ nunjucks.configure('views', {
     express: app,
 });
 
+app.use(bodyParser.urlencoded({ extended: true }))  
+
 app.use('/', indexRouter);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql2');
 
-router.get('/', async function (req, res) {
-    res.send('Hello you!')
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
 });
 
+const promisePool = pool.promise();
+
 module.exports = router;
+
+
+router.get('/', async function (req, res, next) {
+    const [rows] = await promisePool.query("SELECT * FROM eho02forum");
+    res.render('index.njk', {
+        rows: rows,
+        title: 'Forum',
+    });
+});
+
+
+router.post('/new', async function (req, res, next) {
+    console.log(req)
+    const { title, content } = req.body;
+    const author = 1;
+    const [rows] = await promisePool.query("INSERT INTO eho02forum (authorId, title, content) VALUES (?, ?, ?)", [author, title, content]);
+    res.redirect('/');
+});
+
+router.get('/new', async function (req, res, next) {
+    const [users] = await promisePool.query("SELECT * FROM eho02users");
+    res.render('new.njk', {
+        title: 'Nytt inlägg',
+        users,
+    });
+});
+
